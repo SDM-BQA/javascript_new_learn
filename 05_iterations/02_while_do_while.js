ac9b7ecbe3d9b2dc607b8859f628a4b9f0532c69
SDQ-BQA <sdmbqa420@gmail.com>
2024-11-28 00:37:17 +0530

    while_do_while

---
+++ 05_iterations/02_while_do_while.js
@@ -0,0 +1,16 @@
+let z = 1;
+let i = 10;
+
+// when don't know how many time loop will run
+while (z <= i) {
+  console.log(z);
+  z++;
+}
+
+let isUser = false;
+
+
+// will execute atleast onetime
+do {
+  console.log("hehehehehe");
+} while (isUser);
