14d1f9bc246fd1000bbc29bf54fafe51b883f666
SDQ-BQA <sdmbqa420@gmail.com>
2024-11-27 13:25:46 +0530

    api overview & destructuring of object

---
+++ 02_basics/05_object_destructuring_apis_overview.js
@@ -0,0 +1,24 @@
+const student ={
+    studentName : "sundaram DM",
+    branch:"MCA",
+    hod:"Dr. Gomathi R",
+}
+
+// destructuring
+const {branch, hod} = student
+const {studentName: stud} = student
+
+console.log(stud);
+
+// APIs
+// {
+//     name:"sdm",
+//     courseName:"js",
+//     price:"999",
+// }
+
+// [
+//     {},
+//     {},
+//     {}
+// ]
