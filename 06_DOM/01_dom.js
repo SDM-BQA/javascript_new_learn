f836c4b80ce3dc86582d645ef7bb9ef11922c275
SDQ-BQA <sdmbqa420@gmail.com>
2024-11-30 01:03:55 +0530

    01_dom_basics

---
+++ 06_DOM/01_dom.js
@@ -0,0 +1,14 @@
+// Document Object Model
+
+// const title = document.getElementById('title').id
+
+// console.log(title);
+
+// innerText -> visible text
+// textContent -> whole text
+// innerHTML -> whole html
+// document.querySelector -> will select the first element
+// document.querySelectorAll -> will select all the element
+
+// we can't iterate htmlcollection
+// we need to convert it to array using Array.from
